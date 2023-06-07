a38fefd393e0baf1bce571102cef10210995e988
takumu <takumu155695@gmail.com>
2023-06-07 22:57:08 +0900

    Fix: 再度インデント修正

--- model/user.go
+++ model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Email     string    `json:"email" gorm:"unique"`
-	Name     string     `json:"name"`
+	Name      string     `json:"name"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
