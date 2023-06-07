198aec25ca20b7fdc6eaa81cc4b69b213998cf13
takumu <takumu155695@gmail.com>
2023-06-07 22:48:29 +0900

    Add: UserモデルとTodoモデルを作成

---
+++ model/todo.go
@@ -0,0 +1,21 @@
+package model
+
+import "time"
+
+type Todo struct {
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	Title       string    `json:"title" gorm:"not null"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	User        User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
+	UserId      uint      `json:"user_id" gorm:"not null"`
+}
+
+type TodoResponse struct {
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	Title       string    `json:"title" gorm:"not null"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
