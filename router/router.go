fc37fcfcc5662400bf22e5a0471ab5c28668cce2
takumu <takumu155695@gmail.com>
2023-06-19 17:51:03 +0900

    Add: todoに関するrouterを設定

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITodoController) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
@@ -28,6 +28,17 @@ func NewRouter(uc controller.IUserController) *echo.Echo {
 		TokenLookup: "cookie:token",
 	}))
 	u.PUT("/rename", uc.ReName)
+
+	t := e.Group("/todos")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", tc.GetAllTodos)
+	t.GET("/:todoId", tc.GetTodoById)
+	t.POST("/createTodo", tc.CreateTodo)
+	t.PUT("/:todoId", tc.EditTodo)
+	t.DELETE("/:todoId", tc.DeleteTodo)
 
 	return e
 }
