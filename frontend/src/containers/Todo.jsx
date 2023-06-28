f5496c89872322887b935c846ad06160bc5f7989
takumu <takumu155695@gmail.com>
2023-06-29 07:42:53 +0900

    Add: Todoリストの表示

--- frontend/src/containers/Todo.jsx
+++ frontend/src/containers/Todo.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Axioslogout } from '../apis/user/logout'
 import { useNavigate } from 'react-router-dom'
 import { Axiostodos } from '../apis/todos'
 
 export const Todo = () => {
   const navigate = useNavigate()
+  const [todos, setTodos] = useState([]);
 
   const submitLogoutHandler = async (e) => {
     e.preventDefault()
@@ -19,17 +20,31 @@ export const Todo = () => {
     const data = async () => {
       try {
         const result = await Axiostodos();
-        console.log(result);
+        setTodos(result)
       } catch (error) {
         console.log(error);
       }
     };
     data();
-  })
+  },[])
 
   return(
     <>
       <div>Todoリスト</div>
+      <div>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo.id}>
+              <div>
+                {todo.title}
+              </div>
+              <div>
+                {todo.description}
+              </div>
+            </div>
+          )
+        })}
+      </div>
       <div>
         <button onClick={submitLogoutHandler}>
           ログアウト
